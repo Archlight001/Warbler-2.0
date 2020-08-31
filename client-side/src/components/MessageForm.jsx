ebdea4ba60d9aa6af9991e5484c4d1857cf6ad87
Archlight001 <archlight001@gmail.com>
2020-08-31 10:15:08 +0100

    Make message component functional

--- client-side/src/components/MessageForm.jsx
+++ client-side/src/components/MessageForm.jsx
@@ -1,18 +1,42 @@
 import React from "react";
-import {connect} from "react-redux"
+import { connect } from "react-redux";
+import { postNewMessage } from "../store/actions/messages";
+import { useForm } from "react-hook-form";
 
-function MessageForm(props){
-    return (
-        <div>
-            "Message Form"
-        </div>
-    )
+function MessageForm(props) {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    props.postNewMessage(data.message);
+    props.history.push("/");
+  };
+
+  const style ={float:"right"}
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {props.errors.message || errors.message  && (
+        <div className="alert alert-danger">{props.errors.message || errors.message.message}</div>
+      )}
+
+      <input
+        type="text"
+        className="form-control"
+        name="message"
+        id="message"
+        ref={register({ required: "Please input a new Message" })}
+      />
+      <button type="submit" style={style} className="btn btn-success">
+        Add my message!
+      </button>
+    </form>
+  );
 }
 
-function mapReduxStatetoProps(state){
-    return {
-        errors:state.errors
-    }
+function mapReduxStatetoProps(state) {
+  return {
+    errors: state.errors,
+  };
 }
 
-export default connect(mapReduxStatetoProps)(MessageForm);
+export default connect(mapReduxStatetoProps, { postNewMessage })(MessageForm);
