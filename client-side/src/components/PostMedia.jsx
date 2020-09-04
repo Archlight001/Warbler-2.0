16437bec3a2d99165795dd8e7888fa845b62331f
Archlight001 <archlight001@gmail.com>
2020-09-04 12:07:11 +0100

    Add video attachment to post on post timeline

--- client-side/src/components/PostMedia.jsx
+++ client-side/src/components/PostMedia.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
-export default function PostMedia({ postMedia,stretch }) {
+export default function PostMedia({ postMedia, stretch }) {
+  let mediaExtension = postMedia.slice(postMedia.lastIndexOf("."));
+  let mediaType = "image";
+
+  if (
+    [
+      ".mp4",
+      ".webm",
+      ".mpg",
+      ".mp2",
+      ".mpeg",
+      ".mpe",
+      ".mpv",
+      ".ogg",
+      ".avi",
+    ].includes(mediaExtension)
+  ) {
+    mediaType = "video";
+  }
   return (
-    <div style={stretch && {width:"100%"}}>
-      <img src={`http://${postMedia}`} height="250" width="400" alt="" />
+    <div style={stretch && { width: "100%" }}>
+      {mediaType === "image" ? (
+        <img src={`http://${postMedia}`} height="250" width="400" alt="" />
+      ) : (
+        <video width="320" height="240" src={`http://${postMedia}`} controls />
+      )}
     </div>
   );
 }
