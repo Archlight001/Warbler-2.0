64ddaab72176516aefac88878f5d9b502429a10b
Archlight001 <archlight001@gmail.com>
2021-03-11 13:17:50 +0100

    Fix unclicakble button in home page

--- client-side/src/components/Homepage.jsx
+++ client-side/src/components/Homepage.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import "../css/Homepage.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import PostTimeline from "../containers/PostTimeline";
 
-
-export default function Homepage({currentUser,sidebar,showSidebar}) {
-  if(!currentUser.isAuthenticated){
+export default function Homepage({ currentUser, sidebar, showSidebar }) {
+  if (!currentUser.isAuthenticated) {
     return (
       <div className="home-hero">
         <h1>What's Happenning?</h1>
         <h4>New to Warbler?</h4>
+
         <Link to="/signup" className="btn btn-primary">
           Sign up here
         </Link>
       </div>
     );
-  }else{
+  } else {
     return (
       <div>
-        <PostTimeline sidebar={sidebar} showSidebar={showSidebar} profileImage ={currentUser.user.profileImage} username={currentUser.user.username} />
+        <PostTimeline
+          sidebar={sidebar}
+          showSidebar={showSidebar}
+          profileImage={currentUser.user.profileImage}
+          username={currentUser.user.username}
+        />
       </div>
-    )
+    );
   }
 }
